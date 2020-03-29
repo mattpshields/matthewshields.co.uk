61a018efcc026d7f0831d877406e5b0c59faaad0
Matthew Shields <matthew.shields@stickyeyes.com>
2020-03-29 23:52:22 +0100

    Bring component selection logic into FlexibleContent component

--- src/components/FlexibleContent/FlexibleContent.jsx
+++ src/components/FlexibleContent/FlexibleContent.jsx
@@ -1,30 +1,28 @@
 import React from "react";
-import SectionSelector from "./SectionSelector";
+import TextBlock from "../TextBlock/TextBlock";
 
 class FlexibleContent extends React.Component {
-  getPostList() {
-    const postList = [];
-    this.props.postEdges.forEach(postEdge => {
-      postList.push({
-        path: postEdge.node.fields.slug,
-        url: postEdge.node.frontmatter.link_url,
-        cover: postEdge.node.frontmatter.cover,
-        title: postEdge.node.frontmatter.title,
-        date: format_date(postEdge.node.frontmatter.date),
-        excerpt: postEdge.node.excerpt,
-        timeToRead: postEdge.node.timeToRead,
-        short_description: postEdge.node.frontmatter.short_description
-      });
-    });
-    return postList;
+
+  choose_section(section) {
+    switch(section.type) {
+      case 'text-block':
+        return (
+          <TextBlock title={section.title} text={section.text} />
+        )
+        break;
+      default:
+        // code block
+    }
   }
 
   render() {
     let sections = this.props.sections;
     return (
       <div>
-        {sections.map(section => (
-          <SectionSelector section={section} />
+        {sections.map((section, index) => (
+          <div key={section.type+'_'+index}>
+            {this.choose_section(section)}
+          </div>
         ))}
       </div>
     );
